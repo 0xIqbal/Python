d4e303c704c0d4cea2d36342ceaf994f515228d4
IQBAL BIN HAFIZ <hafiz2305341073@diu.edu.bd>
2026-05-15 20:42:48 +0600

    3

---
+++ 3.py
@@ -0,0 +1,16 @@
+age = 19
+can_vote = age>=18
+print(can_vote)
+a,b,c = "Orange","Banana","Cherry"
+print (a)
+print (b)
+print (c)
+
+a=b=c= "orange"
+print("a= ",a)
+
+#Unpack a Collection
+fruits = ["apple","banana","cherry"]
+x,y,z = fruits
+print(fruits)
+print(x)
