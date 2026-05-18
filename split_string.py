0ecfb04fdb4e98d83d58c0a367ec9cd2b7db487e
IQBAL BIN HAFIZ <hafiz2305341073@diu.edu.bd>
2026-05-18 18:33:06 +0600

    new

---
+++ split_string.py
@@ -0,0 +1,8 @@
+"""The split() method splits the string 
+into substrings if it finds 
+instances of the separator:"""
+
+a = "I am Iqbal"
+print(a.split("am")) 
+# here "am" is a instance. when it find
+# this it well separate it 
