5cf2a7f0fbfe987f64edd13405fd649fac639f04
IQBAL BIN HAFIZ <hafiz2305341073@diu.edu.bd>
2026-05-18 18:33:56 +0600

    Stringformat

---
+++ Stringformat.py
@@ -0,0 +1,15 @@
+"""
+String Format
+As we learned in the Python Variables chapter, 
+we cannot combine 
+strings and numbers like this:
+Example
+age = 36
+#This will produce an error:
+txt = "My name is John, I am " + age
+print(txt)"""
+
+#F-Strings
+age = 20
+text =f"I am Iqbal . I am {age} years old"
+print(text)
