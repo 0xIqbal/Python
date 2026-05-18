026e3cd0cdd7e758601015bac1c33bb0f9304b77
IQBAL BIN HAFIZ <hafiz2305341073@diu.edu.bd>
2026-05-18 18:33:22 +0600

    n

---
+++ String_Slicing.py
@@ -0,0 +1,22 @@
+"""
+We can get range of character from 
+a sentence by slice syntax
+a[initial num : End Number]
+
+"""
+a = "Hello I am Iqbal"
+print(a[4:12]) 
+
+# if you wanna print 4 to 11 we have to enter (end_number+1)
+
+#printing 1st to last 
+print(a[0:16])
+
+#print 5 to last 
+print(a[5:])
+
+#print 1st to 5
+print(a[:5])
+
+#negative index 
+print(a[-5:])
