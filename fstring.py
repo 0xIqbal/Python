689b28fd965b1e9fb18f1d4b7cb4adecc41302fb
IQBAL BIN HAFIZ <hafiz2305341073@diu.edu.bd>
2026-05-19 20:23:30 +0600

    fstring

---
+++ fstring.py
@@ -0,0 +1,15 @@
+"""
+1. A placeholder can include a modifier to 
+format the value.Display the price with
+2 decimals.
+
+2. Perform a math operation in the 
+placeholder, and return the result:
+"""
+price =20
+text = f"The price is {price:.2f} dollars"
+print(text)
+
+#2
+txt = f"the price is {20*3:.2f} dollars"
+print(txt)
