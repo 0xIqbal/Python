656ccbd593e8dee26e2c6c2312172e7e1b76281f
IQBAL BIN HAFIZ <hafiz2305341073@diu.edu.bd>
2026-05-19 20:23:57 +0600

    my video

---
+++ vedio1.py
@@ -0,0 +1,51 @@
+#Iqbal
+#Welcome to my youtube channel
+
+# comment 
+"""
+I am a student .
+I can do better from you 
+"""
+# capitalize
+a = "i am Iqbal"
+print(a.capitalize())
+
+#2nd fstring
+
+"""
+string = "Iqbal bin hafiz age = "
+result = age +string
+print(result)
+"""
+age = 25
+name = "Arnob"
+
+text = f"Hello, I am Arnob . and MY age is {age}"
+print(text)
+
+#3rd slicing 
+
+b = "I am iqbal"
+print(b[2:6])
+
+#center
+c = "Arnob"
+print(c.center(50))
+
+#finding a string 
+print("Index of Arnob",text.find("Arnob"))
+
+#Upper and lower case 
+print(text.upper())
+print(text.lower())
+
+#Strip()
+t = "     I have a dream     "
+print(t.strip())
+
+#con
+s = " Hello, this a youtube video"
+v = " I made it for update my skills. update speaking skill"
+
+re = s+v
+print(re)
