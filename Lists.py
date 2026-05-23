24a4e619dbd3e33a7444fb49227ef9f5ce5c07ae
IQBAL BIN HAFIZ <hafiz2305341073@diu.edu.bd>
2026-05-24 00:17:18 +0600

    Lists

---
+++ Lists.py
@@ -0,0 +1,25 @@
+"""
+Lists are used to store multiple items in 
+a single variable.
+List items are ordered, changeable, and
+allow duplicate values.
+"""
+
+List = ["Apple","Banana","Cherry","Apple"]
+print(List)
+print("Len = ",len(List))
+
+#String,int and boolean data type
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+print(list1)
+print(list2)
+print(list3)
+
+#A list with strings, integers and boolean values:
+List4 = [1,2,1.4,"Apple",True]
+print(List4)
+#Type of list 
+print(type(List4))
