ab1c6dc77e5627b0910913359da75ffd86f7d026
IQBAL BIN HAFIZ <hafiz2305341073@diu.edu.bd>
2026-05-25 23:07:35 +0600

    remove

---
+++ removefromlist.py
@@ -0,0 +1,9 @@
+#Remove Specified Item
+#The remove() method removes the specified item.
+"""If there are more than one 
+item with the specified value, the remove() 
+method removes the first occurrence:
+"""
+list = ["apple","Banana","Iqbal","hellow","world","Iqbal"]
+list.remove("Iqbal")
+print("After remove: ",list)
