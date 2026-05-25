c646545cecec888a8ae73e48824dc66b76a4d9c9
IQBAL BIN HAFIZ <hafiz2305341073@diu.edu.bd>
2026-05-25 22:03:18 +0600

    insertvalue

---
+++ insertvaluesinlist.py
@@ -0,0 +1,9 @@
+"""
+To insert a new list item, 
+without replacing any of the existing values,
+we can use the insert() method.
+"""
+#insert nana as 3 value
+list = ["Iqbal","Imran","Gawhar"]
+list.insert(2,"nana")
+print(list)
