5504ca82381bea22f426d9d8279eaf9803cbd94f
IQBAL BIN HAFIZ <hafiz2305341073@diu.edu.bd>
2026-05-25 22:03:06 +0600

    changelist1

---
+++ changelist1.py
@@ -0,0 +1,6 @@
+#Change the second and third value by
+#  replacing it with one value:
+list = ["Banana","Arnob","Nana","Hlw"]
+print("Print without change  = ",list)
+list[1:3]= ["Watermelon"]
+print("After change = ",list)
