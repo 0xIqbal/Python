89b99be7c33406b71a3999b93f089e0f44e5c8c8
IQBAL BIN HAFIZ <hafiz2305341073@diu.edu.bd>
2026-05-25 21:35:27 +0600

    changelist

---
+++ changeList.py
@@ -0,0 +1,15 @@
+list = ["apple","Banana","Cherry"]
+list[1] = "Arnob"
+print(list)
+
+"""
+Change the values "banana" and "cherry" 
+with the values 
+"blackcurrant" and "watermelon":
+"""
+#         0        1          2    we took end value 
+# 3 bcz endvalue = endvalue index+1 
+list1 =["apple", "banana", "cherry",
+         "orange", "kiwi", "mango"]
+list1[1:3] = ["blackcurrant","Watermelon"]
+print(list1)
