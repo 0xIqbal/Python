41db653bf5b157f58d023be902c9d3a6a7f1c857
IQBAL BIN HAFIZ <hafiz2305341073@diu.edu.bd>
2026-05-25 22:12:50 +0600

    extend

---
+++ extend.py
@@ -0,0 +1,10 @@
+#Extend list
+"""To append elements from another 
+list to the current list, 
+use the extend() method."""
+
+l1 =["Apple","Banana","cherry"]
+l2 = ["mango","pineapple","papaya"]
+print("Before Extending: ",l1)
+l1.extend(l2)
+print("After extending: ",l1)
