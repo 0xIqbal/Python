e028dbc1242b6a7434193ccf5703b1ea77d44307
IQBAL BIN HAFIZ <hafiz2305341073@diu.edu.bd>
2026-06-02 11:30:36 +0600

    loop

---
+++ loop.py
@@ -0,0 +1,18 @@
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+    print(x)
+
+for x in range(len(thislist)):
+    print(thislist[x])
+
+for x in range(5):
+    print("Hello world")
+
+for x in range(5):
+    print(x)
+
+for i in range(1,6):
+    print(i)
+
+for j in range(0,20,3):
+    print (j)
