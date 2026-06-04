23fc484f9b8459467fcad62556643f45f774d0bb
IQBAL BIN HAFIZ <hafiz2305341073@diu.edu.bd>
2026-06-04 22:42:28 +0600

    com

---
+++ com.py
@@ -0,0 +1,13 @@
+human = ["Misti","Mariam","Sabiha","ArzU","dipty"]
+new = []
+for a in human:
+    if "r" in a :
+        new.append(a)
+
+print(new)
+
+# we can do it in one line.
+# [expression for item in list if condition]
+
+newnew = [x for x in human if "i" in x]
+print(newnew)
