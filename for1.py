58ca3a0403b2f7bc6aab9c8e78ce840c3ec02bcb
IQBAL BIN HAFIZ <hafiz2305341073@diu.edu.bd>
2026-06-13 23:49:49 +0600

    for1

---
+++ for1.py
@@ -0,0 +1,15 @@
+name= "Iqbal"
+for i in range(5):
+    print(name)
+
+#while loop
+count = 1
+while count<=5:
+    print(name)
+    count += 1
+
+
+for i in range (10):
+    if i ==4:
+        continue
+    print(i)
