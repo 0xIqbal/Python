a3b4adb5f11405afec26698e438785d63f85e8e1
IQBAL BIN HAFIZ <hafiz2305341073@diu.edu.bd>
2026-06-13 23:50:42 +0600

    slicing

---
+++ slicinglab.py
@@ -0,0 +1,10 @@
+number = [1,2,3,10,30,40,19,11]
+print(number[2:6])
+number.append(3000)
+print("append: ",number)
+number.remove(3)
+print("remove: ",number)
+
+#Sort use for sorting smaller to larger
+number.sort()
+print("sort: ",number)
