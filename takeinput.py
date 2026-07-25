c7cc2daa0b9e8477e25fbcb4da4166c307407cc2
IQBAL BIN HAFIZ <hafiz2305341073@diu.edu.bd>
2026-07-25 23:09:21 +0600

    takeinput

---
+++ takeinput.py
@@ -0,0 +1,7 @@
+#we can take input by "input()"
+Name = input("Enter your name: ")
+age = input("Enter your age: ")
+CG = input("Enter your cg: ")
+print("Your name is ",Name)
+print("Your age is ",age)
+print("Your cg is ",CG)
